4486511cd2c65da3788e8a25c45ce3792cc435e2
PiotrFiedoruk <fiedorukpiotr@gmail.com>
2021-02-22 12:43:17 +0100

    2.1 Dashboard pf

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -9,3 +9,10 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
+
+class MainView(View):
+
+    def get(self, request):
+        ctx = {}
+        return render(request, 'dashboard.html', ctx)
