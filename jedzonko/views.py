4216e32e588297e87fc301d44452fccd146c800e
MariuszLuczak <mariuszluczak@op.pl>
2021-02-22 14:40:58 +0100

    zrobione dodawanie przepisu, poprawić linki

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from jedzonko.models import Recipe
+from django.http import HttpRequest, HttpResponse
 
 # doałem komentarz testowy
 
@@ -28,3 +30,17 @@ class RecipeAddView(View):
     def get(self,request):
         return render(request,'app-add-recipe.html')
 
+    def post(self,request):
+        ingr = request.POST.get("ingredients")
+        prep_desc = request.POST.get("preparation_description")
+        prep_time = int(request.POST.get("preparation_time"))
+        recipe_desc = request.POST.get("recipe_description")
+        recipe_name = request.POST.get("recipe_name")
+        #
+        #
+        # return HttpResponse(f"{ingr}, {prep_desc}, {prep_time}, {recipe_desc}, {recipe_name}")
+        if ingr != '' and prep_time != '' and prep_desc != '' and recipe_desc != '' and recipe_name != '':
+            Recipe.objects.create(name=recipe_name, ingredients=ingr,preparation_time=prep_time, description=prep_desc, description_short=recipe_desc)
+            return redirect('dashboard.html')
+        else:
+            return redirect('app-add-recipe.html')
