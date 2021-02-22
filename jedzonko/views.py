d4ca90498dd19739e2068819a563aeb913716497
Lisikm <lisikm@gmail.com>
2021-02-22 16:29:45 +0100

    Recipe list

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from jedzonko.models import Recipe
 from random import shuffle
+from django.core.paginator import Paginator
 
 # doałem komentarz testowy
 
@@ -11,6 +12,7 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
+
 
 class MainView(View):
 
@@ -24,39 +26,51 @@ class LandingPageView(View):
     def get(self, request):
         recipes = list(Recipe.objects.all())
         shuffle(recipes)
-        ctx = {"recipes":recipes}
+        ctx = {"recipes": recipes}
         return render(request, "index.html", ctx)
 
-      
+
 class RecipeAddView(View):
     def get(self, request):
         return render(request, 'app-add-recipe.html')
+
 
 class RecipeDetails(View):
     def get(self, request):
         return render(request, 'app-recipe-details.html')
 
+
 class RecipeListView(View):
     def get(self, request):
-        return render(request, 'app-recipes.html')
+        recipes_list = Recipe.objects.all().order_by("-votes", "created")
+        paginator = Paginator(recipes_list, 50)
+        page = request.GET.get('page')
+        recipes = paginator.get_page(page)
+        ctx = {"recipes":recipes}
+        return render(request, 'app-recipes.html', ctx)
+
 
 class RecipeModifyView(View):
     def get(self, request):
         return render(request, 'app-edit-recipe.html')
 
-#Plans
+
+# Plans
 
 class PlanListView(View):
     def get(self, request):
         return render(request, 'app-schedules.html')
 
+
 class PlanDetailsView(View):
     def get(self, request):
         return render(request, 'app-details-schedules.html')
 
+
 class PlanAddView(View):
     def get(self, request):
         return render(request, 'app-add-schedules.html')
+
 
 class PlanAddRecipeView(View):
     def get(self, request):
